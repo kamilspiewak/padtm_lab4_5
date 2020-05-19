7a7cea35fa51a58d810d7552b2973bf2800c3b75
Kamil <kamilspiewak97@gmail.com>
2020-05-19 17:56:58 +0200

    done lab4

--- app/src/main/java/com/example/padtm_lab4_5/MainActivity.java
+++ app/src/main/java/com/example/padtm_lab4_5/MainActivity.java
@@ -44,11 +44,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onActivityResult(
-        int requestCode, int resultCode,Intent data){
-        if(requestCode==1 && resultCode==RESULT_OK){
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
-            String nowy = (String)extras.get("wpis");
+            String nowy = (String) extras.get("wpis");
             target.add(nowy);
             adapter.notifyDataSetChanged();
         }
